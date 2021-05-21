ea9b29266e3e3700e663e46daaad8fee8829552d
Hunter Skrasek <hunterskrasek@me.com>
2021-05-21 13:14:26 -0500

    Update HitsIteratorAggregate interfact and concrete to map hits into Doctrine entities
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- src/ElasticSearch/DoctrineHitsIteratorAggregate.php
+++ src/ElasticSearch/DoctrineHitsIteratorAggregate.php
@@ -1,6 +1,11 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace HSkrasek\LaravelDoctrine\Scout\DoctrineElasticSearch\ElasticSearch;
+
+use Illuminate\Support\Collection;
+use LaravelDoctrine\Scout\SearchableRepository;
 
 class DoctrineHitsIteratorAggregate implements HitsIteratorAggregate
 {
@@ -11,14 +16,41 @@ class DoctrineHitsIteratorAggregate implements HitsIteratorAggregate
      */
     private $callback;
 
-    public function __construct(array $results, callable $callback = null)
+    private ?SearchableRepository $repository;
+
+    public function __construct(array $results, callable $callback = null, ?SearchableRepository $repository = null)
     {
-        $this->results  = $results;
-        $this->callback = $callback;
+        $this->results    = $results;
+        $this->callback   = $callback;
+        $this->repository = $repository;
     }
 
-    public function getIterator()
+    public function getIterator(): \ArrayIterator
     {
-        // TODO: Implement getIterator() method.
+        if ($this->results['hits']['total'] <= 0) {
+            return new \ArrayIterator([]);
+        }
+
+        $hits = $this->results['hits']['hits'];
+
+        $entities = collect($hits)->groupBy('_source.__class_name')
+            ->map(
+                function (Collection $results, $class) {
+                    return $this->repository->whereIn('id', $results->pluck('_id')->all())->get();
+                }
+            )
+            ->flatten()->keyBy(
+                function ($entity) {
+                    return get_class($entity) . '::' . $entity->getScoutKey();
+                }
+            );
+
+        $hits = collect($hits)->map(function ($hit) use ($entities) {
+            $key = $hit['_source']['__class_name'] . '::' . $hit['_id'];
+
+            return $entities[$key] ?? null;
+        })->filter()->all();
+
+        return new \ArrayIterator((array)$hits);
     }
 }
--- src/ElasticSearch/HitsIteratorAggregate.php
+++ src/ElasticSearch/HitsIteratorAggregate.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace HSkrasek\LaravelDoctrine\Scout\DoctrineElasticSearch\ElasticSearch;
 
+use LaravelDoctrine\Scout\SearchableRepository;
+
 interface HitsIteratorAggregate extends \IteratorAggregate
 {
-    public function __construct(array $results, callable $callback = null);
+    public function __construct(array $results, ?callable $callback = null, ?SearchableRepository $repository);
 }
